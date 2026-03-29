d1fbab6524409218898c0fff0431eb9fdfeefd4c
Crawford Huzz <huzz@serious.yeah>
2026-03-29 21:12:47 +0500

    add 4th and 5th chapter

---
+++ fifth/android/app/src/main/kotlin/com/example/fifth/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fifth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ thrd/android/app/src/main/kotlin/com/example/thrd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.thrd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
